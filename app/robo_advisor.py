30b181a8d7c3dac354934c9d2e9ec022944ae4f8
Mike Rossetti <s2t2@users.noreply.github.com>
2019-02-20 15:01:32 -0500

    Issue a request

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,5 +1,27 @@
 # app/robo_advisor.py
 
+import requests
+import json
+
+#
+# INFO INPUTS
+#
+
+request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo"
+
+response = requests.get(request_url)
+# print(type(response)) #> <class 'requests.models.Response'>
+# print(response.status_code) #> 200
+# print(response.text)
+
+parsed_response = json.loads(response.text)
+
+breakpoint()
+
+#
+# INFO OUTPUTS
+#
+
 print("-------------------------")
 print("SELECTED SYMBOL: MSFT")
 print("-------------------------")
