c5f0aeea27c6f4b0cd94370aa20c98a4919f9b06
Mike Rossetti <s2t2@users.noreply.github.com>
2019-02-20 15:20:55 -0500

    Get latest close

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -3,6 +3,11 @@
 import requests
 import json
 
+# utility function to convert float or integer to usd-formatted string (for printing)
+# ... adapted from: https://github.com/s2t2/shopping-cart-screencast/blob/30c2a2873a796b8766e9b9ae57a2764725ccc793/shopping_cart.py#L56-L59
+def to_usd(my_price):
+    return "${0:,.2f}".format(my_price) #> $12,000.71
+
 #
 # INFO INPUTS
 #
@@ -10,13 +15,12 @@ import json
 request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo"
 
 response = requests.get(request_url)
-# print(type(response)) #> <class 'requests.models.Response'>
-# print(response.status_code) #> 200
-# print(response.text)
 
 parsed_response = json.loads(response.text)
 
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+
+latest_close = parsed_response["Time Series (Daily)"]["2019-02-20"]["4. close"] #> $1,000.00
 
 #breakpoint()
 
@@ -31,7 +35,7 @@ print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
-print("LATEST CLOSE: $1,000.00")
+print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print("RECENT HIGH: $1,111.11")
 print("RECENT LOW: $999.99")
 print("-------------------------")
