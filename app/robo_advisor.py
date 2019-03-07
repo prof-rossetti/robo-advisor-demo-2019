968683c20ac0dea5e263b0c3d67447ca0049efe6
Mike Rossetti <s2t2@users.noreply.github.com>
2019-03-07 14:59:28 -0500

    Introduce modularity

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -8,94 +8,96 @@ import os
 from dotenv import load_dotenv
 import requests
 
-load_dotenv() #> loads contents of the .env file into the script's environment
-
 # utility function to convert float or integer to usd-formatted string (for printing)
 # ... adapted from: https://github.com/s2t2/shopping-cart-screencast/blob/30c2a2873a796b8766e9b9ae57a2764725ccc793/shopping_cart.py#L56-L59
 def to_usd(my_price):
     return "${0:,.2f}".format(my_price) #> $12,000.71
 
-#
-# INFO INPUTS
-#
+if __name__ == "__main__":
 
-time_now = datetime.datetime.now() #> datetime.datetime(2019, 3, 3, 14, 44, 57, 139564)
+    load_dotenv() #> loads contents of the .env file into the script's environment
 
-# ASSEMBLE REQUEST URL
+    #
+    # INFO INPUTS
+    #
 
-api_key = os.environ.get("ALPHAVANTAGE_API_KEY", "demo") # default to using the "demo" key if an Env Var is not supplied
+    time_now = datetime.datetime.now() #> datetime.datetime(2019, 3, 3, 14, 44, 57, 139564)
 
-symbol = "AMZN" # TODO: accept user input
+    # ASSEMBLE REQUEST URL
 
-request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}"
+    api_key = os.environ.get("ALPHAVANTAGE_API_KEY", "demo") # default to using the "demo" key if an Env Var is not supplied
 
-# ISSUE REQUEST AND PARSE RESPONSE
+    symbol = "AMZN" # TODO: accept user input
 
-response = requests.get(request_url)
-parsed_response = json.loads(response.text)
-metadata = parsed_response["Meta Data"]
-tsd = parsed_response["Time Series (Daily)"]
+    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}"
 
-last_refreshed = metadata["3. Last Refreshed"]
+    # ISSUE REQUEST AND PARSE RESPONSE
 
-# TRANSFORM DATA INTO A MORE FAMILIAR / USABLE STRUCTURE (LIST OF DICTIONARIES :-D)
+    response = requests.get(request_url)
+    parsed_response = json.loads(response.text)
+    metadata = parsed_response["Meta Data"]
+    tsd = parsed_response["Time Series (Daily)"]
 
-rows = []
+    last_refreshed = metadata["3. Last Refreshed"]
 
-for date, daily_prices in tsd.items(): # see: https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/notes/python/datatypes/dictionaries.md
-    row = {
-        "timestamp": date,
-        "open": float(daily_prices["1. open"]),
-        "high": float(daily_prices["2. high"]),
-        "low": float(daily_prices["3. low"]),
-        "close": float(daily_prices["4. close"]),
-        "volume": int(daily_prices["5. volume"])
-    }
-    rows.append(row)
+    # TRANSFORM DATA INTO A MORE FAMILIAR / USABLE STRUCTURE (LIST OF DICTIONARIES :-D)
 
-latest_close = rows[0]["close"]
+    rows = []
 
-high_prices = [row["high"] for row in rows] # list comprehension for mapping purposes!
-low_prices = [row["low"] for row in rows] # list comprehension for mapping purposes!
-recent_high = max(high_prices)
-recent_low = min(low_prices)
+    for date, daily_prices in tsd.items(): # see: https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/notes/python/datatypes/dictionaries.md
+        row = {
+            "timestamp": date,
+            "open": float(daily_prices["1. open"]),
+            "high": float(daily_prices["2. high"]),
+            "low": float(daily_prices["3. low"]),
+            "close": float(daily_prices["4. close"]),
+            "volume": int(daily_prices["5. volume"])
+        }
+        rows.append(row)
 
-#
-# INFO OUTPUTS
-#
+    latest_close = rows[0]["close"]
 
-# WRITE PRICES TO CSV FILE
-# see: https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/notes/python/modules/csv.md#writing-csv-files
-# see: https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/notes/python/modules/os.md#file-operations
+    high_prices = [row["high"] for row in rows] # list comprehension for mapping purposes!
+    low_prices = [row["low"] for row in rows] # list comprehension for mapping purposes!
+    recent_high = max(high_prices)
+    recent_low = min(low_prices)
 
-csv_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
-csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
+    #
+    # INFO OUTPUTS
+    #
 
-with open(csv_filepath, "w") as csv_file:
-    writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
-    writer.writeheader() # uses fieldnames set above
-    for row in rows:
-        writer.writerow(row)
+    # WRITE PRICES TO CSV FILE
+    # see: https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/notes/python/modules/csv.md#writing-csv-files
+    # see: https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/notes/python/modules/os.md#file-operations
 
-# DISPLAY RESULTS
+    csv_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
+    csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
 
-formatted_time_now = time_now.strftime("%Y-%m-%d %H:%M:%S") #> '2019-03-03 14:45:27'
+    with open(csv_filepath, "w") as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
+        writer.writeheader() # uses fieldnames set above
+        for row in rows:
+            writer.writerow(row)
 
-formatted_csv_filepath = csv_filepath.split("../")[1] #> data/prices.csv
+    # DISPLAY RESULTS
 
-print("-------------------------")
-print(f"SYMBOL: {symbol}")
-print("-------------------------")
-print(f"REQUEST AT: {formatted_time_now}")
-print(f"REFRESH DATE: {last_refreshed}")
-print("-------------------------")
-print(f"RECENT HIGH:  {to_usd(recent_high)}")
-print(f"LATEST CLOSE: {to_usd(latest_close)}")
-print(f"RECENT LOW:   {to_usd(recent_low)}")
-print("-------------------------")
-print("RECOMMENDATION: TODO") # TODO
-print("BECAUSE: TODO") # TODO
-print(f"WRITING DATA TO CSV: {formatted_csv_filepath}")
-print("-------------------------")
-print("HAPPY INVESTING!")
-print("-------------------------")
+    formatted_time_now = time_now.strftime("%Y-%m-%d %H:%M:%S") #> '2019-03-03 14:45:27'
+
+    formatted_csv_filepath = csv_filepath.split("../")[1] #> data/prices.csv
+
+    print("-------------------------")
+    print(f"SYMBOL: {symbol}")
+    print("-------------------------")
+    print(f"REQUEST AT: {formatted_time_now}")
+    print(f"REFRESH DATE: {last_refreshed}")
+    print("-------------------------")
+    print(f"RECENT HIGH:  {to_usd(recent_high)}")
+    print(f"LATEST CLOSE: {to_usd(latest_close)}")
+    print(f"RECENT LOW:   {to_usd(recent_low)}")
+    print("-------------------------")
+    print("RECOMMENDATION: TODO") # TODO
+    print("BECAUSE: TODO") # TODO
+    print(f"WRITING DATA TO CSV: {formatted_csv_filepath}")
+    print("-------------------------")
+    print("HAPPY INVESTING!")
+    print("-------------------------")
