fe0f33e3d717d8ab8645b3a23350c9ca76131803
Mike Rossetti <s2t2@users.noreply.github.com>
2019-03-07 16:02:25 -0500

    Fix failing test

--- test/advisor_test.py
+++ test/advisor_test.py
@@ -1,7 +1,4 @@
 from app.robo_advisor import to_usd
 
-def test_something():
-    assert (2 + 2) == 5
-
 def test_to_usd():
-    assert to_usd(123456.8) == "$123,456.70"
+    assert to_usd(123456.8) == "$123,456.80"
